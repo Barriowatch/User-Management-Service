bc67d0bb349f89abcf278c5dac563d6abaeaa4fb
Sankara Ekane <sankaraekane@Sankaras-MBP.lan1>
2020-05-06 21:20:11 -0400

    BWA-63: Create Update enpoint for Personal Provider service "PUT /api/providers" added  with controller test and Model methods tests

--- src/controllers/personalProvider.controller.js
+++ src/controllers/personalProvider.controller.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 const PersonalProviders = require('../models/personalProvider.model');
 const userAddedMessage = require('../message-bus/send/user.added');
 
@@ -27,45 +28,12 @@ const PersonalProviderController = {
   },
 
   update: (req, res, next) => {
-    PersonalProviders.findById(req.payload.id).then((user) => {
-      if (!user) { return res.sendStatus(401); }
-
-      // only update fields that were actually passed...
-      if (typeof req.body.user.lastname !== 'undefined') {
-        PersonalProviders.lastname = req.body.user.lastname;
-      }
-      if (typeof req.body.user.firstname !== 'undefined') {
-        PersonalProviders.firstname = req.body.user.firstname;
-      }
-      if (typeof req.body.user.email !== 'undefined') {
-        PersonalProviders.email = req.body.user.email;
-      }
-      if (typeof req.body.user.phone !== 'undefined') {
-        PersonalProviders.phone = req.body.user.phone;
-      }
-      if (typeof req.body.user.status !== 'undefined') {
-        PersonalProviders.status = req.body.user.status;
-      }
-      if (typeof req.body.user.profile_picture !== 'undefined') {
-        PersonalProviders.profile_picture = req.body.user.profile_picture;
-      }
-      if (typeof req.body.user.birth_date !== 'undefined') {
-        PersonalProviders.birth_date = req.body.user.birth_date;
-      }
-      if (typeof req.body.user.gender !== 'undefined') {
-        PersonalProviders.gender = req.body.user.gender;
-      }
-      if (typeof req.body.user.barrio_id !== 'undefined') {
-        PersonalProviders.barrio_id = req.body.user.barrio_id;
-      }
-      if (typeof req.body.user.location !== 'undefined') {
-        PersonalProviders.location = req.body.user.location;
-      }
-      if (typeof req.body.user.password !== 'undefined') {
-        PersonalProviders.setPassword(req.body.user.password);
-      }
-
-      return user.save().then(() => res.json({ user }));
+    const promise = PersonalProviders.findByIdAndUpdate(
+      req.body.user.id,
+      req.body.user,
+    );
+    promise.then((user) => {
+      res.json({ user });
     }).catch(next);
   },
 
@@ -74,7 +42,7 @@ const PersonalProviderController = {
       if (!user) { return res.sendStatus(401); }
 
 
-      return req.user.remove().then(() => res.sendStatus(204)).catch(next);
+      return user.remove().then(() => res.sendStatus(204)).catch(next);
     });
   },
 
